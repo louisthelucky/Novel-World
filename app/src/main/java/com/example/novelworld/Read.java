b36a3f659e137b5eed82033e7caced57d76cf13a
Novel World <michael18ricafrente@gmail.com>
2023-10-03 01:39:30 +0800

    Final Commit

--- app/src/main/java/com/example/novelworld/Read.java
+++ app/src/main/java/com/example/novelworld/Read.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ShareCompat;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,7 +19,13 @@ public class Read extends AppCompatActivity {
     public ImageView novelpic;
     public TextView chap;
 
+    public EditText comment;
 
+    public static final String EXTRA_MESSAGE = "com.example.android.novelworld.extra.MESSAGE";
+
+    public static final int TEXT_REQUEST = 1;
+
+    private static final String LOG_TAG = Read.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +44,44 @@ public class Read extends AppCompatActivity {
         novelpic.setImageResource(imageResource);
         chap.setText(description);
 
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        comment = findViewById(R.id.commentText);
+        comment.setText("");
+        Log.d(LOG_TAG, "onStop");
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
     }
 
 
@@ -44,13 +89,24 @@ public class Read extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), mes, Toast.LENGTH_SHORT).show();
     }
     public void heartClk (View view) {
-        displayToast("You been heart the Novel");
+        displayToast("You heart the Novel: " + title.getText());
     }
-    public void comClk (View view) {
-        displayToast("comment");
-        Intent intent = new Intent(Read.this, Comment.class);
-        startActivity(intent);
+    public void comClk(View view) {
+        comment = findViewById(R.id.commentText);
+
+        String message = comment.getText().toString();
+
+        // Check if the message is not empty or null
+        if (!message.isEmpty()) {
+            Intent intent = new Intent(this, Comment.class);
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivityForResult(intent, TEXT_REQUEST);
+        } else {
+            // Display a toast message if there is no comment
+            Toast.makeText(this, "Please enter a comment", Toast.LENGTH_SHORT).show();
+        }
     }
+
     public void shareClk (View view) {
         String txt = getIntent().getStringExtra("titleKey");
         String mimeType = "text/plain";
